19a8205c1df13ad8a9fd16d277f82fe1faa24b12
Utikeles <142643953+Utikeles@users.noreply.github.com>
2025-06-10 21:50:27 -0400

    Update universalCmdClient.lua
    Add support for command line history indexing with up arrow and down arrow, also adds support for re-entering protocol for rednet.

--- remoteTools/misc/universalCmdClient.lua
+++ remoteTools/misc/universalCmdClient.lua
@@ -12,13 +12,68 @@ if not modemSide then
     return
 end
 
+::start::
+
 rednet.open(modemSide)
 write("Enter protocol name: ")
 local protocol = read()
 
+local cmdList = {}
+local i = 1
+
 while true do
     write("> ")
-    local cmd = read()
-    if cmd == "exit" then break end
-    rednet.broadcast(cmd, protocol)
+    term.setCursorBlink(true)
+    local input = ""
+    local historyIndex = i
+    local xPrompt, yPrompt = term.getCursorPos()
+    while true do
+        local event, param = os.pullEvent()
+        if event == "char" then
+            input = input .. param
+            write(param)
+        elseif event == "key" then
+            if param == keys.enter then
+                print()
+                break
+            elseif param == keys.up then
+                if historyIndex > 1 and cmdList[historyIndex - 1] then
+                    local _, y = term.getCursorPos()
+                    term.setCursorPos(1, y)
+                    term.clearLine()
+                    historyIndex = historyIndex - 1
+                    input = cmdList[historyIndex]
+                    write("> " .. input)
+                end
+            elseif param == keys.down then
+                local _, y = term.getCursorPos()
+                term.setCursorPos(1, y)
+                term.clearLine()
+                if cmdList[historyIndex + 1] then
+                    historyIndex = historyIndex + 1
+                    input = cmdList[historyIndex]
+                    write("> " .. input)
+                else
+                    historyIndex = i
+                    input = ""
+                    write("> ")
+                end
+            elseif param == keys.backspace then
+                if #input > 0 then
+                    input = input:sub(1, -2)
+                    local x, y = term.getCursorPos()
+                    term.setCursorPos(x - 1, y)
+                    write(" ")
+                    term.setCursorPos(x - 1, y)
+                end
+            end
+        end
+    end
+    term.setCursorBlink(false)
+    cmdList[i] = input
+    if input == "exit" then cmdList = {} break end
+    rednet.broadcast(input, protocol)
+    i = i + 1
 end
+
+goto start
